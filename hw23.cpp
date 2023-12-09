00fdfe5f8c503fe4e1f4115f1213cf0faa4435b0
bob dylan kinnie <135342698+olenakiriakida@users.noreply.github.com>
2023-12-09 20:08:20 +0200

    Add files via upload

---
+++ hw23.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() 
+{
+    set<int> tree;
+
+    tree.insert(5);
+    tree.insert(10);
+    tree.insert(15);
+    tree.insert(20);
+    tree.insert(25);
+
+    cout << "Дерево до видалення елементів: ";
+    for (int elem : tree) 
+    {
+        cout << elem << " ";
+    }
+    cout << "\n";
+
+    tree.erase(10);
+    tree.erase(20);
+
+    cout << "Дерево після видалення елементів: ";
+    for (int elem : tree) 
+    {
+        cout << elem << " ";
+    }
+    cout << "\n";
+
+}
